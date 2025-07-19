4c3cfd5db93bf83b302fb567733883d9f569441e
Palak Srivastava <110525851+palak2609@users.noreply.github.com>
2025-07-19 15:07:03 +0530

    Initial Files added 2

--- src/main/java/com/ConnectSphere/Backend/controller/testController.java
+++ src/main/java/com/ConnectSphere/Backend/controller/testController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
-    @GetMapping("/abc")
+    @GetMapping("/")
     public String home() {
         return "Spring Boot is Working Fine!";
     }
